3b62761243ed73fe81af1410f1ac07bf65b78422
mufid1012 <mufidghibran1012@gmail.com>
2024-05-28 15:49:22 +0700

    menambakan main untuk remotelampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -15,3 +15,21 @@ public:
 		return saklarNo[i];
 	}
 };
+
+int main()
+{
+	remotelampu lampuRumah;
+
+	lampuRumah.setsaklarNo(0, "Lampu Teras Rummah");
+	lampuRumah.setsaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setsaklarNo(2, "Lampu kamar Tidur");
+	lampuRumah.setsaklarNo(3, "Lampu dapur ");
+
+	cout << lampuRumah.getsaklarNo(0) << endl;
+	cout << lampuRumah.getsaklarNo(1) << endl;
+	cout << lampuRumah.getsaklarNo(2) << endl;
+	cout << lampuRumah.getsaklarNo(3) << endl;
+
+	return 0;
+
+}
