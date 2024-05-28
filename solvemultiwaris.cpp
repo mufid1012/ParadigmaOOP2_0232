e72e750fa3c88f853202f8d1ea82fdf932653b54
mufid1012 <mufidghibran1012@gmail.com>
2024-05-28 14:47:40 +0700

    membuat class orang dengan atribut constructor umur

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class Orang
+{
+public:
+    int umur;
+
+    Orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
