918f382a67aa61da489ac957be71121762445a92
mufid1012 <mufidghibran1012@gmail.com>
2024-05-28 15:48:20 +0700

    menambahkan kelas remote lampu  dengan getter dan setter untuk saklar

---
+++ enkapsulasi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class remotelampu {
+private:
+	string saklarNo[10];
+
+public:
+	void setsaklarNo(int i, string value)
+	{
+		saklarNo[i] = value;
+	}
+	string getsaklarNo(int i)
+	{
+		return saklarNo[i];
+	}
+};
